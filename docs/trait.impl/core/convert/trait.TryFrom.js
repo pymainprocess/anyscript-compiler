a42189ee9bcc4f56d4d60178b831ad5b4444bec4
pymainprocess <pymainprocess@outlook.com>
2024-06-29 20:52:28 +0200

    Update

---
+++ docs/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"base64":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"base64/alphabet/struct.Alphabet.html\" title=\"struct base64::alphabet::Alphabet\">Alphabet</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
