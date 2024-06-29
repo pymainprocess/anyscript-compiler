57d4e0f07a9613755ac4f5d2e19a88ab10a6c784
pymainprocess <pymainprocess@outlook.com>
2024-06-29 13:22:32 +0200

    Update

--- src/types/float.rs
+++ src/types/float.rs
@@ -1,3 +1,10 @@
-pub struct Float {
-    pub value: f64,
+use crate::types::prelude::*;
+use crate::alias::*;
+use std::ffi::CString;
+
+pub struct Float<T1> {
+    pub number: FloatAlias,
+    pub original: T1,
+    pub size: IntAlias,
+    pub type_id: String,
 }
